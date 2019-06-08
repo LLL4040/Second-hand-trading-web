bb79d7c941a0adfcea69899faf898350221a4cf4
rzzhimo <1070634876@qq.com>
2019-06-08 20:29:40 +0800

    init backstage

---
+++ second-hand-tranding-backstage/src/main/java/secondhandtrandingbackstage/secondhandtrandingbackstage/SecondHandTrandingBackstageApplication.java
@@ -0,0 +1,13 @@
+package secondhandtrandingbackstage.secondhandtrandingbackstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHandTrandingBackstageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHandTrandingBackstageApplication.class, args);
+    }
+
+}
---
+++ second-hand-tranding-backstage/src/test/java/secondhandtrandingbackstage/secondhandtrandingbackstage/SecondHandTrandingBackstageApplicationTests.java
@@ -0,0 +1,16 @@
+package secondhandtrandingbackstage.secondhandtrandingbackstage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecondHandTrandingBackstageApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
